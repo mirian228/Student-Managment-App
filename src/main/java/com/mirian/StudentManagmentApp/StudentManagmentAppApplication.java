24737d8d688bc3f9f74a07f15bba10f08d9f3db6
mirian228 <pariksini@mail.ru>
2023-06-04 16:26:22 +0400

    Project startpoint

---
+++ src/main/java/com/mirian/StudentManagmentApp/StudentManagmentAppApplication.java
@@ -0,0 +1,13 @@
+package com.mirian.StudentManagmentApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagmentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagmentAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mirian/StudentManagmentApp/StudentManagmentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mirian.StudentManagmentApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagmentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
